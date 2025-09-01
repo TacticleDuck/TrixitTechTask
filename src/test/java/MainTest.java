18398a1f6a571714ac2cf71652d142621780eaac
Mirek <trixit@example.com>
2025-09-01 19:05:47 +0200

    basic steps added to check functionality to MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,21 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class MainTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private final By searchBar = By.cssSelector("[name='q']");
 
     @BeforeMethod
     public void setup() {
-        System.out.println("setup");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @Test
     public void test() {
-        System.out.println("test");
+        driver.get("https://www.google.com/");
+        wait.until(ExpectedConditions.presenceOfElementLocated(searchBar));
     }
 
     @AfterMethod
     public void cleanup() {
-        System.out.println("cleanup");
+        driver.quit();
     }
 }
