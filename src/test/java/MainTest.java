e809f331c15944b48d6b24bdfe144cf60ce147e5
Mirek <trixit@example.com>
2025-09-01 18:55:41 +0200

    basic testng structure added to MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,21 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class MainTest {
+
+    @BeforeMethod
+    public void setup() {
+        System.out.println("setup");
+    }
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+
+    @AfterMethod
+    public void cleanup() {
+        System.out.println("cleanup");
+    }
 }
